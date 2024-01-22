cad64d19aaf1ddcd745c34e6f13429e989d4c4b7
theindresh <indreshbharati77@gmail.com>
2024-01-23 03:04:10 +0530

    first commit

---
+++ src/BasicPrograms/AddTwoNumbers.java
@@ -0,0 +1,9 @@
+package BasicPrograms;
+
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 25;
+        System.out.println("Sum of "+a+ " and "+b+" is = "+(a+b));
+    }
+}
